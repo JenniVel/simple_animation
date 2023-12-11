ac3e5684d4ebc33cf7ff2cc1815158fad37634d0
JenniVel <118595533+JenniVel@users.noreply.github.com>
2023-12-11 14:13:59 +0700

    baru

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animation/widgets/animated_container.dart';
+import 'package:simple_animation/widgets/animated_cross_fade.dart';
+import 'package:simple_animation/widgets/animated_magnifier.dart';
+import 'package:simple_animation/widgets/animated_opacity.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Animation Controller'),
+          backgroundColor: Colors.blue[100],
+        ),
+        body: const Center(
+          child: AnimatedMagnifier(),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple Animation'),
+        backgroundColor: Colors.blue[100],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            AnimatedContainerWidget(),
+            Divider(),
+            AnimatedCrossFadeWidget(),
+            Divider(),
+            AnimatedOpacityWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
